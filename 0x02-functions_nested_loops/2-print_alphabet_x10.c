9a3702e2bb12afcbd010d3e5fea44170b4e3d6f1
umar <omarsabiu679@gmail.com>
2022-06-07 16:53:33 -0700

    fhfhfhfh

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * print_aplhabet_x10 - short description
- * Return: Always 0
+ * print_aplhabet_x10 - Prints the alphabet 10 times.
+ *
+ * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
