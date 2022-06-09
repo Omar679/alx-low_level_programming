7b8796e388a73ea74d70c81930fe4433e206e955
umar <omarsabiu679@gmail.com>
2022-06-09 05:14:16 -0700

    message

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,34 +1,29 @@
 #ifndef _holberton_h_
 #define _holberton_h_
+int _isupper(int c);
 
-int _putchar(char c);
+int _isdigit(int c);
+
+int mul(int a, int b);
+
+void print_numbers(void);
 
 int _putchar(char);
-void print_alphabet_x10(void);
 
-void print_alphabet(void);
+void print_most_numbers(void);
 
-void print_alphabet_x10(void);
+void more_numbers(void);
 
-int _islower(int c);
+void print_line(int n);
 
-int _isalpha(int c);
+void print_diagonal(int n);
 
-int print_sign(int n);
+void print_square(int size);
 
-int _abs(int);
+void print_triangle(int size);
 
-int print_last_digit(int);
+void print_number(int n);
 
-void jack_bauer(void);
-
-void times_table(void);
-
-int add(int, int);
-
-void print_to_98(int n);
-
-void print_times_table(int n);
 
 
 
