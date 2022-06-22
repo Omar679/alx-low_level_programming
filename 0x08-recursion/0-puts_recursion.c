172c7cc5264f7df2728dc0f8cdc7c63f1b940d85
umar <omarsabiu679@gmail.com>
2022-06-22 13:26:27 -0700

    Message

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,13 +2,13 @@
 
 /**
  * _puts_recursion - prints a string, followed by a new line.
- * @s: string
+ * @s: strin
  * Return: no return.
  */
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
-	{
+			{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
