8826e937df7696863a1ee62097afc24111d4d045
umar <omarsabiu679@gmail.com>
2022-07-29 13:48:31 -0700

    Submission 1

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <sys/types.h>
+
+#include <sys/stat.h>
+
+#include <fcntl.h>
+
+#include <unistd.h>
+
+#include <elf.h>
+
+/**
+ * print_addr - prints address
+ * @ptr: magic.
+ * Return: no return.
+ */
+void print_addr(char *ptr)
+{
+	int i;
+	int begin;
+	char sys;
+
+	printf("  Entry point address:               0x");
+
+	sys = ptr[4] + '0';
+	if (sys == '1')
+	{
+		begin = 26;
+		printf("80");
+		for (i = begin; i >= 22; i--)
+		{
+
