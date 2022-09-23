b079e3df13ff851e3982ee2b310a0f290d3c5235
umar <omarsabiu679@gmail.com>
2022-09-23 13:28:59 -0700

    sshshd

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: The hash table
+ * @key: The key
+ * @value: The value
+ *
+ * Return: int
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node;
+
+	if (ht == NULL)
+		return (0);
+	if (ht->size == 0 || ht->array == NULL)
+		return (0);
+	if (key == NULL || key[0] == ' ' || strcmp(key, "") == 0)
+		return (0);
+	if (value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
